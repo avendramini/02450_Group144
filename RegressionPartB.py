24db9a5acb1d4ebd620572bd834e1ecc543d8964
Alberto Vendramini <alberto.vendra@gmail.com>
2023-11-11 19:32:27 +0100

    Point 3 Reggression Part B

--- RegressionPartB.py
+++ RegressionPartB.py
@@ -12,7 +12,7 @@ from toolbox_02450 import rlr_validate, train_neural_net, draw_neural_net
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 import torch
-from scipy import stats
+from scipy import stats as st
 
 
 def rlr2_validate(X, y, lambdas, hs, cvf=10):
@@ -49,7 +49,7 @@ def rlr2_validate(X, y, lambdas, hs, cvf=10):
     BL_means = np.empty(cvf)
     f = 0
     for train_index, test_index in CV.split(X, y):
-        print(f"INTERNAL CROSS-VALIDATION, FOLD NUMBER: {f}")
+        #print(f"INTERNAL CROSS-VALIDATION, FOLD NUMBER: {f}")
         X_train = X[train_index]
         y_train = y[train_index]
         X_test = X[test_index]
@@ -105,7 +105,7 @@ def rlr2_validate(X, y, lambdas, hs, cvf=10):
                                                                X=X_train2,
                                                                y=y_train2,
                                                                n_replicates=1,
-                                                               max_iter=1000)
+                                                               max_iter=5000)
             y_test_est = net(X_test2)
             se = (y_test_est.float()-y_test2.float())**2  # squared error
             mse = (sum(se).type(torch.float)/len(y_test2)).data.numpy()  # mean
@@ -178,33 +178,43 @@ K = 10
 CV = model_selection.KFold(n_splits=K, shuffle=True)
 
 lambdas = np.power(10., range(-5, 9))
-hs=np.array([5,10,15,20])
+#hs=np.array([1,2,3,4,5])
+hs=np.array([10])
 # Initialize variables
 #T = len(lambdas)
 Error_train = np.empty((K, 1))
 Error_test = np.empty((K, 1))
 GLR_train_err = np.empty((K, 1))
 GLR_test_err = np.empty((K, 1))
+GLR_opt_lambda = np.empty(K)
 GBL_train_err = np.empty((K, 1))
 GBL_test_err = np.empty((K, 1))
 GANN_train_err = np.empty((K, 1))
 GANN_test_err = np.empty((K, 1))
+GANN_opt_h=np.empty(K)
 w_rlr = np.empty((M, K))
 mu = np.empty((K, M-1))
 sigma = np.empty((K, M-1))
 w_noreg = np.empty((M, K))
 
+CI_BLvsLR=np.empty(K,dtype=object)
+CI_BLvsANN=np.empty(K,dtype=object)
+CI_ANNvsLR=np.empty(K,dtype=object)
+p_BLvsLR=np.empty((K,1))
+p_BLvsANN=np.empty((K,1))
+p_ANNvsLR=np.empty((K,1))
+
 k = 0
 for train_index, test_index in CV.split(X, y):
-    print(f"EXTERNAL CROSS-VALIDATION, FOLD NUMBER: {k}")
+    #print(f"EXTERNAL CROSS-VALIDATION, FOLD NUMBER: {k}")
     # extract training and test set for current CV fold
     X_train = X[train_index]
     y_train = y[train_index]
     X_test = X[test_index]
     y_test = y[test_index]
-    internal_cross_validation = 10
+    internal_cross_validation = 5
     # CHANGE TO EVALUATE ALL 3 MODELS
-    LR_opt_val_err, LR_opt_lambda, LR_mean_w_vs_lambda, LR_train_err_vs_lambda, LR_test_err_vs_lambda, ANN_opt_val_err, ANN_opt_h, ANN_train_err_vs_h, ANN_test_err_vs_h,BL_opt_val_err,BL_opt_mean,BL_train_error,BL_test_error = rlr2_validate(
+    LR_opt_val_err, GLR_opt_lambda[k], LR_mean_w_vs_lambda, LR_train_err_vs_lambda, LR_test_err_vs_lambda, ANN_opt_val_err, ANN_opt_h, ANN_train_err_vs_h, ANN_test_err_vs_h,BL_opt_val_err,BL_opt_mean,BL_train_error,BL_test_error = rlr2_validate(
         X_train, y_train, lambdas,hs, internal_cross_validation)
 
     # Standardize outer fold based on training set, and save the mean and standard
@@ -219,16 +229,17 @@ for train_index, test_index in CV.split(X, y):
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
     
-    
     GBL_train_err[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
     GBL_test_err[k] = np.square(y_test-y_train.mean()).sum(axis=0)/y_test.shape[0]
-    
-    lambdaI = LR_opt_lambda * np.eye(M)
+    BL_y_test_est=np.full(y_test.shape[0], y_train.mean())
+    lambdaI = GLR_opt_lambda[k]* np.eye(M)
     lambdaI[0,0] = 0 # Do no regularize the bias term
     w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
     # Compute mean squared error with regularization with optimal lambda
+    LR_y_test_est=X_test @ w_rlr[:,k]
     GLR_train_err[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
-    GLR_test_err[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
+    GLR_test_err[k] = np.square(y_test-LR_y_test_est).sum(axis=0)/y_test.shape[0]
+    
     
     def model(): return torch.nn.Sequential(
         torch.nn.Linear(M, ANN_opt_h),  # M features to n_hidden_units
@@ -251,9 +262,9 @@ for train_index, test_index in CV.split(X, y):
                                                        X=X_train2,
                                                        y=y_train2,
                                                        n_replicates=1,
-                                                       max_iter=1000)
-    y_test_est = net(X_test2)
-    se = (y_test_est.float()-y_test2.float())**2  # squared error
+                                                       max_iter=5000)
+    ANN_y_test_est = net(X_test2)
+    se = (ANN_y_test_est.float()-y_test2.float())**2  # squared error
     mse = (sum(se).type(torch.float)/len(y_test2)).data.numpy()  # mean
     GANN_test_err[k] = mse[0]
 
@@ -265,33 +276,51 @@ for train_index, test_index in CV.split(X, y):
     # store error rate for current CV fold
     GANN_train_err[k] = train_mse[0]
     
+    GANN_opt_h[k]=ANN_opt_h
+
+
+
+    #COMPARISON
+    zBL=np.abs(y_test - BL_y_test_est ) ** 2
+    zLR=np.abs(y_test-LR_y_test_est)**2
+    zANN=se.detach().numpy().squeeze()
+    
+    alpha = 0.05
+    CIBL = st.t.interval(1-alpha, df=len(zBL)-1, loc=np.mean(zBL), scale=st.sem(zBL))# Confidence interval    
+    CILR = st.t.interval(1-alpha, df=len(zLR)-1, loc=np.mean(zLR), scale=st.sem(zLR))
+    CIANN = st.t.interval(1-alpha, df=len(zANN)-1, loc=np.mean(zANN), scale=st.sem(zANN))
+    
+    #BL VS LR
+    z = zBL - zLR
+    CI_BLvsLR[k] = st.t.interval(1-alpha, len(z)-1, loc=np.mean(z), scale=st.sem(z))  # Confidence interval
+    p_BLvsLR[k] = 2*st.t.cdf( -np.abs( np.mean(z) )/st.sem(z), df=len(z)-1)  # p-value
+    
+    #BL VS ANN
+    z = zBL - zANN
+    CI_BLvsANN[k] = st.t.interval(1-alpha, len(z)-1, loc=np.mean(z), scale=st.sem(z))  # Confidence interval
+    p_BLvsANN[k] = 2*st.t.cdf( -np.abs( np.mean(z) )/st.sem(z), df=len(z)-1)  # p-value
+    
+    #ANN VS LR
+    z = zANN - zLR
+    CI_ANNvsLR[k] = st.t.interval(1-alpha, len(z)-1, loc=np.mean(z), scale=st.sem(z))  # Confidence interval
+    p_ANNvsLR[k] = 2*st.t.cdf( -np.abs( np.mean(z) )/st.sem(z), df=len(z)-1)  # p-value
+    
+    
     
     k += 1
     
-
+for k in range(0,K):
+    print(f"FOLD: {k+1} h: {GANN_opt_h[k]} E_ann: {GANN_test_err[k]} lambda: {GLR_opt_lambda[k]} E_lr: {GLR_test_err[k]}")
 plt.show()
-plt.title("Artificial Neural Network")
 plt.xlabel("Fold")
 plt.ylabel("MSE")
-plt.plot(range(0,10),GANN_train_err,label="Train error")
-plt.plot(range(0,10),GANN_test_err,label="Test error")
-plt.legend()
-plt.show()
+plt.plot(range(0,K),GANN_train_err,label="ANN Train error")
+plt.plot(range(0,K),GANN_test_err,label="ANN Test error")
 
-plt.show()
-plt.title("Linear Regression")
-plt.xlabel("Fold")
-plt.ylabel("MSE")
-plt.plot(range(0,10),GLR_train_err,label="Train error")
-plt.plot(range(0,10),GLR_test_err,label="Test error")
-plt.legend()
-plt.show()
+plt.plot(range(0,K),GLR_train_err,label="LR Train error")
+plt.plot(range(0,K),GLR_test_err,label="LR Test error")
 
-plt.show()
-plt.title("Baseline")
-plt.xlabel("Fold")
-plt.ylabel("MSE")
-plt.plot(range(0,10),GBL_train_err,label="Train error")
-plt.plot(range(0,10),GBL_test_err,label="Test error")
+plt.plot(range(0,K),GBL_train_err,label="BL Train error")
+plt.plot(range(0,K),GBL_test_err,label="BL Test error")
 plt.legend()
 plt.show()
